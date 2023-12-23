f6282eb721552a4961122f061a458a959d7c6471
Olha75 <137830839+Olha75@users.noreply.github.com>
2023-12-23 21:38:10 +0200

    restr

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,43 @@
+// import user from '/user.json';
+// import { Profiler } from 'react';
+
+export const Profile = ({
+  views,
+  followers,
+  stats,
+  avatar,
+  location,
+  tag,
+  username,
+  likes,
+}) => {
+  return (
+    <div class="profile">
+      <div class="description">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+          alt="User avatar"
+          class="avatar"
+        />
+        <p class="name">Petra Marica</p>
+        <p class="tag">@pmarica</p>
+        <p class="location">Salvador, Brasil</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">1000</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">2000</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">3000</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+export * from 'components/App/App.jsx';
+export * from 'components/Profile/Profile.jsx';
+export * from 'components/Statistics/Statistics.jsx';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-
+import { App } from 'components/App/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
